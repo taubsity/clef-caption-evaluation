8167a70a8aaa55603ed32b1a8b634f4d94bae494
Tabea Pakull <tabea.pakull@uk-essen.de>
2024-12-02 18:02:20 +0100

    Refactor CaptionEvaluator to improve scoring methods and enhance error handling

--- caption_prediction/evaluator.py
+++ caption_prediction/evaluator.py
@@ -4,6 +4,7 @@ import warnings
 import numpy as np
 import re
 import evaluate
+from tqdm import tqdm
 
 # IMAGECLEF 2025 CAPTION - CAPTION PREDICTION
 class CaptionEvaluator:
@@ -31,27 +32,42 @@ class CaptionEvaluator:
 
 
     def _evaluate(self, client_payload, _context={}):
+        # """
+        # This is the only method that will be called by the framework
+        # returns a _result_object that can contain up to 2 different scores
+        # `client_payload["submission_file_path"]` will hold the path of the submission file
+        # """
         """
-        This is the only method that will be called by the framework
-        returns a _result_object that can contain up to 2 different scores
-        `client_payload["submission_file_path"]` will hold the path of the submission file
+        This method that will be called by the framework returns a _result_object that can contains 
+        different scores. `client_payload["submission_file_path"]` will hold the path of the submission file.
         """
         print("evaluate...")
         # Load submission file path
         submission_file_path = client_payload["submission_file_path"]
-        # Load preditctions and validate format
+        # Load predictions and validate format
         predictions = self.load_predictions(submission_file_path)
 
-        bleu_score = self.compute_primary_score(predictions)
-        score_secondary = self.compute_secondary_score(predictions)
+        bertscore = self.compute_bertscore(predictions)
+        rouge = self.compute_rouge(predictions)
+        sim = 0
+        medcon = 0
+        alignscore = 0
+
+        # _result_object = {
+        #     "score": bert,
+        #     "score_secondary": rouge
+        # }
 
         _result_object = {
-            "score": bleu_score,
-            "score_secondary": score_secondary
+            "bert": bertscore,
+            "rouge": rouge,
+            "similarity": sim,
+            "medcon": medcon,
+            "align": alignscore
         }
 
-        assert "score" in _result_object
-        assert "score_secondary" in _result_object
+        # assert "score" in _result_object
+        # assert "score_secondary" in _result_object
 
         return _result_object
 
@@ -65,15 +81,7 @@ class CaptionEvaluator:
         with open(self.ground_truth_path) as csvfile:
             reader = csv.reader(csvfile)
 
-            # Check if the first line is a header and skip it
-            first_line = next(reader)
-
-            if "ID" in first_line[0]:
-                pass
-            else:
-                pairs[first_line[0]] = first_line[1]
-
-            for row in reader:
+            for row in tqdm(reader):
                 pairs[row[0]] = row[1]
         return pairs
 
@@ -86,61 +94,45 @@ class CaptionEvaluator:
 
         pairs = {}
         image_ids_gt = set(self.gt.keys())
+        occured_images = []
+        lineCnt = 0
         with open(submission_file_path) as csvfile:
             reader = csv.reader(csvfile)
 
-            lineCnt = 0
-            occured_images = []
-            for row in reader:
-                if "ID" not in row[0]:
-                    lineCnt += 1
-
-                    # less than two pipe separated tokens on line => Error
-                    if(len(row) < 2):
-                        self.raise_exception("Wrong format: Each line must consist of an image ID followed by a ',' (comma) and a caption ({}).",
-                                                lineCnt, "<imageID><comma><caption>")
-
-                        image_id = row[0]
-
-                        # Image ID does not exist in testset => Error
-                        if image_id not in image_ids_gt:
-                            self.raise_exception(
-                                "Image ID '{}' in submission file does not exist in testset.", lineCnt, image_id)
-                            # raise Exception("Image ID '{}' in submission file does not exist in testset {}"
-                            #     .format(image_id,self.line_nbr_string(lineCnt)))
-
-                        # image id occured at least twice in file => Error
-                        if image_id in occured_images:
-                            self.raise_exception(
-                                "Image ID '{}' was specified more than once in submission file.", lineCnt, image_id)
-                            # raise Exception("Image ID '{}' was specified more than once in submission file {}"
-                            #     .format(image_id, self.line_nbr_string(lineCnt)))
-
-                        occured_images.append(image_id)
-
-                        pairs[row[0]] = row[1]
-
-                # In case not all images from the testset are contained in the file => Error
-                if(len(occured_images) != len(image_ids_gt)):
+            for row in tqdm(reader):
+                # less than two pipe separated tokens on line => Error
+                if (len(row) < 2):
+                    self.raise_exception("Wrong format: Each line must consist of an image ID followed by a ',' (comma) and a caption ({}).",
+                                            lineCnt, "<imageID><comma><caption>")
+                
+                # Image ID does not exist in testset => Error
+                image_id = row[0]
+                if (image_id not in image_ids_gt):
                     self.raise_exception(
-                        "Number of image IDs in submission file not equal to number of image IDs in testset.", lineCnt)
+                        "Image ID '{}' in submission file does not exist in testset.", lineCnt, image_id)
 
-            return pairs
+                # image id occured at least twice in file => Error
+                if (image_id in occured_images):
+                    self.raise_exception(
+                        "Image ID '{}' was specified more than once in submission file.", lineCnt, image_id)
+                
+                
+                pairs[row[0]] = row[1]
+            
+                occured_images.append(image_id)
+                lineCnt += 1
+
+        # In case not all images from the testset are contained in the file => Error
+        if(len(occured_images) != len(image_ids_gt)):
+            self.raise_exception(
+                f"Number of image IDs in submission file not equal to number of image IDs in testset.\nNumber in testset: {len(image_ids_gt)}\nNumber in submission: {len(occured_images)}", lineCnt)
+
+            
+        return pairs
 
     def raise_exception(self, message, record_count, *args):
         raise Exception(message.format(
             *args)+" Error occured at record line {}.".format(record_count))
-
-    def compute_primary_score(self, predictions):
-        """
-        Compute and return the primary score
-        `predictions` : valid predictions in correct format
-        NO VALIDATION OF THE RUNFILE SHOULD BE IMPLEMENTED HERE
-        Valiation should be handled in the load_predictions method
-        """
-        print("compute primary score...")
-
-        return self.compute_bertscore(predictions)
 
     def compute_bertscore(self, candidate_pairs):
         # Hide warnings
@@ -186,25 +178,11 @@ class CaptionEvaluator:
             except Exception as e:
                 print(e)
                 #raise Exception('Problem with {} {}', gt_caption, candidate_caption)
-            bert_scores.append(bert_score["f1"])
+            bert_scores.append(bert_score["recall"])
 
         return np.mean(bert_scores)
 
-    def compute_secondary_score(self, predictions):
-        """
-        Compute and return the secondary score
-        Ignore or remove this method if you do not have a secondary score to provide
-        `predictions` : valid predictions in correct format
-        NO VALIDATION OF THE RUNFILE SHOULD BE IMPLEMENTED HERE
-        Valiation should be handled in the load_predictions method
-        """
-        print("compute secondary score...")
 
-        return self.compute_rouge(predictions)
-
-        # PUT AUXILIARY METHODS BELOW
-        # ...
-        # ...
     def compute_rouge(self, candidate_pairs):
         # Hide warnings
         warnings.filterwarnings('ignore')
@@ -259,15 +237,14 @@ class CaptionEvaluator:
 
 # TEST THIS EVALUATOR
 if __name__ == "__main__":
+    ground_truth_path = "/home/tabea/projects/clef-caption-evaluation/ImageCLEFmedical_2024_Caption/valid_captions.csv"
 
-    ground_truth_path = "/home/tabea/projects/clef-caption-evaluation/ImageCLEFmedical_Caption_test_gt/caption_prediction_test_gt.csv"
-
-    submission_file_path = "/home/tabea/projects/clef-caption-evaluation/ImageCLEFmedical_Caption_test_gt/caption_prediction_test_gt.csv"
+    submission_file_path = "/home/tabea/projects/clef-caption-evaluation/ImageCLEFmedical_2024_Caption/valid_captions.csv"
 
     _client_payload = {}
     _client_payload["submission_file_path"] = submission_file_path
 
-    # Instaiate a dummy context
+    # Instantiate a dummy context
     _context = {}
 
     # Instantiate an evaluator
